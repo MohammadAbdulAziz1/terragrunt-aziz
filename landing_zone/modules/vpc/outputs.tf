7c329ffd55bc3ebb9bc8bade72ffbead7dfd81ca
MohammadAbdulAziz1 <aziz@tivonaglobal.com>
2024-06-13 10:32:24 +0530

    made some changes to landing_zone module

--- landing_zone/modules/vpc/outputs.tf
+++ landing_zone/modules/vpc/outputs.tf
@@ -1,19 +1,26 @@
+# output "vpcs_output" {
+#   value = { for k, v in google_compute_network.vpc_network : k => v.name }
+# }
+# output "subnets_output" {
+#   value = { for k, v in module.subnets : k => v }
+# }
+# output "ip_cidr_range" {
+#   value = { for k, v in module.subnets : k => v }
+# }
+# output "router" {
+#   value = { for k, v in module.subnets : k => v }
+# }
+# output "nat" {
+#   value = { for k, v in module.subnets : k => v }
+# }
+# output "firewall_rules" {
+#   value = { for k, v in module.subnets : k => v }
+# }
+
 output "vpcs_output" {
-  value = { for k, v in google_compute_network.vpc_network : k => v.name }
+  value = { for k, v in google_compute_network.vpc_network : k => v.name}
 }
 output "subnets_output" {
-  value = { for k, v in module.subnets : k => v }
-}
-output "ip_cidr_range" {
-  value = { for k, v in module.subnets : k => v }
-}
-output "router" {
-  value = { for k, v in module.subnets : k => v }
-}
-output "nat" {
-  value = { for k, v in module.subnets : k => v }
-}
-output "firewall_rules" {
   value = { for k, v in module.subnets : k => v }
 }
 
